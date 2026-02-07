92abd2faca64ded332634c99d4807882975d7b8a
Dan Yeh <chenchen.yeh0423@gmail.com>
2026-02-08 01:04:41 +0800

    cleanup

--- mlx/backend/metal/scaled_dot_product_attention.cpp
+++ mlx/backend/metal/scaled_dot_product_attention.cpp
@@ -658,6 +658,8 @@ void quant_sdpa_vector_2pass(
   kname += get_type_string(q.dtype());
   kname += "_";
   kname += std::to_string(q.shape(-1));
+  kname += "_";
+  kname += std::to_string(q.shape(-1));
 
   int N = k.shape(2);
   int gqa_factor = q.shape(1) / k.shape(1);
@@ -728,6 +730,7 @@ void quant_sdpa_vector_2pass(
 
   auto& compute_encoder = d.get_command_encoder(s.index);
   auto kernel = d.get_kernel(kname, hash_name, func_consts);
+  check_kernel_threadgroup_size(kernel, group_dims, hash_name);
   compute_encoder.set_compute_pipeline_state(kernel);
 
   compute_encoder.set_input_array(q, 0);
@@ -772,7 +775,7 @@ void quant_sdpa_vector_2pass(
   kname += "sdpa_vector_2pass_2_";
   kname += get_type_string(q.dtype());
   kname += "_";
-  kname += std::to_string(q.shape(-1));
+  kname += std::to_string(out.shape(-1));
 
   func_consts = {
       {&blocks, MTL::DataType::DataTypeInt, 26},
@@ -790,6 +793,7 @@ void quant_sdpa_vector_2pass(
 
   group_dims = MTL::Size(1024, 1, 1);
   grid_dims = MTL::Size(q.shape(0) * q.shape(1), q.shape(2), 1);
+  check_kernel_threadgroup_size(kernel, group_dims, kname);
   compute_encoder.dispatch_threadgroups(grid_dims, group_dims);
 }
 
@@ -866,10 +870,13 @@ bool QuantizedScaledDotProductAttention::use_fallback(
   }
 
   int query_sequence_length = q.shape(2);
+  int key_sequence_length = k.shape(2);
   int query_head_dim = q.shape(-1);
   int gqa_factor = q.shape(1) / k.shape(1);
   return query_sequence_length > 8 ||
-      !(query_head_dim == 64 || query_head_dim == 128) ||
+      query_sequence_length > key_sequence_length ||
+      !(query_head_dim == 64 || query_head_dim == 128 ||
+        query_head_dim == 256) ||
       (query_sequence_length * gqa_factor > 32);
 }
 
@@ -1125,6 +1132,7 @@ void QuantizedScaledDotProductAttention::eval_gpu(
     o.set_data(allocator::malloc(o.nbytes()));
   }
 
+  bool do_causal = do_causal_ && q.shape(2) > 1;
   quant_sdpa_vector_2pass(
       s,
       d,
@@ -1139,7 +1147,7 @@ void QuantizedScaledDotProductAttention::eval_gpu(
       scale_,
       group_size_,
       bits_,
-      do_causal_,
+      do_causal,
       mask,
       sinks,
       mode_);
